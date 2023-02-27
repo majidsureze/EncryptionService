a53ac02d2109043bfab54c1eafa0fd13fb97c3ec
Majid Shahabfar <Majid@SHAHABFAR>
2023-02-27 10:35:21 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using EncryptionService;
+
+var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+Console.WriteLine("Please enter a string for encryption");
+var str = Console.ReadLine();
+string encryptedString = str.Encrypt(key);
+Console.WriteLine($"encrypted string = {encryptedString}");
+
+var decryptedString = encryptedString.Decrypt(key);
+Console.WriteLine($"decrypted string = {decryptedString}");
+
+
+key = "b14ca5898a4e4133";
+
+encryptedString = str.Encrypt(key);
+Console.WriteLine($"encrypted string = {encryptedString}");
+
+decryptedString = encryptedString.Decrypt(key);
+Console.WriteLine($"decrypted string = {decryptedString}");
+
+Console.ReadKey();
---
+++ EncryptionService/StringExtension.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptionService
+{
+    public static class StringExtension
+    {
+        public static string Encrypt(this string stringToEncrypt, string key)
+        {
+            if (string.IsNullOrEmpty(stringToEncrypt))
+            {
+                throw new ArgumentException("An empty string value cannot be encrypted.");
+            }
+
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentException("Cannot encrypt using an empty key. Please provide an encryption key.");
+            }
+
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using var memoryStream = new MemoryStream();
+                using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+                using (var streamWriter = new StreamWriter(cryptoStream))
+                {
+                    streamWriter.Write(stringToEncrypt);
+                }
+
+                array = memoryStream.ToArray();
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public static string Decrypt(this string stringToDecrypt, string key)
+        {
+            if (string.IsNullOrEmpty(stringToDecrypt))
+            {
+                throw new ArgumentException("An empty string value cannot be encrypted.");
+            }
+
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentException("Cannot decrypt using an empty key. Please supply a decryption key.");
+            }
+
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(stringToDecrypt);
+
+            using Aes aes = Aes.Create();
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = iv;
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using var memoryStream = new MemoryStream(buffer);
+            using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            using var streamReader = new StreamReader(cryptoStream);
+            return streamReader.ReadToEnd();
+        }
+    }
+}
